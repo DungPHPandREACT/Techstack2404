40f8ee9280019326c12efdcc796cb4d9b7249eb7
DungPHPAndReact <dung2000cn@gmail.com>
2024-08-04 17:08:24 +0700

    example darkmode

--- context-use-ref/src/Component7.js
+++ context-use-ref/src/Component7.js
@@ -2,10 +2,15 @@ import React, { useContext } from 'react';
 import DataContext from './contexts/DataContext';
 
 const Component7 = () => {
-	const dataContext = useContext(DataContext);
-	console.log('Giá trị dataContext trong component7 là: ', dataContext);
+	const { data, darkMode, setDarkMode } = useContext(DataContext);
+	console.log('Giá trị dataContext trong component7 là: ', data);
 
-	return <div>Component7</div>;
+	return (
+		<div>
+			<div>Component7</div>
+			<button onClick={() => setDarkMode(!darkMode)}>Set darkMode</button>
+		</div>
+	);
 };
 
 export default Component7;
